b123854a457b97d779fb388725b233fba017f806
EmmaPratt <emma.pratt@student.tdsb.on.ca>
2022-09-16 11:27:57 -0400

    int add

--- Test.java
+++ Test.java
@@ -1,5 +1,12 @@
 public class Test {
-    public static void main(String[] args) {
-    System.out.println("Hello world!");
-   System.out.println("Hello world!");}
+    public static void main(String[] args) 
+    {
+        // printing
+        System.out.println("Hello world!");
+        // printing 
+        System.out.println("Hello world!");
+
+        int num = 5;
+        System.out.println(num);
+    }
 }
